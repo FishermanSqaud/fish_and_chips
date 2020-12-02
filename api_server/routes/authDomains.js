c5fa758af7a80d1abdb619370389033be7a846fa
cs20081607 <37113550+cs20081607@users.noreply.github.com>
2020-12-02 18:37:22 +0000

    Update authDomains.js

--- api_server/routes/authDomains.js
+++ api_server/routes/authDomains.js
@@ -11,25 +11,32 @@ var db = mysql.createConnection({
 })
 db.connect();
 
+// 테이블 authenticated_domains
+// desc authenticated_domains
+
+// 구글 익스텐션 Request.body 키 값을 토큰화 시킨 도메인값을 domain 변수에 저장
 
 
+var domain = 
 
-
-
-
-
+    
+// 도메인명에 해당하는 테이블을 출력. 출력이 되면 논 피싱, 출력이 안되면 피싱
+    
+var sql = 'SELECT ' + domain + ' FROM authenticated_domains';
+db.query(sql, function(err){
+  if(err) {
+    console.log('Fishing');
+  } else {
+    console.log('None Fishing');
+  }
+}
+  
+         
 authDomainsRouter.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   next();
+
 });
-
-
-
-
-
-
-// Usage Example
-// authDomainsRouter.post('/', authDomainsHandler.checkExistence)
 
 
 module.exports = authDomainsRouter;
