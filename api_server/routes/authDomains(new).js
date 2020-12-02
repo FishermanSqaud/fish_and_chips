2ae437a67f863074a20b7ee61766a4e1fa60ca13
cs20081607 <37113550+cs20081607@users.noreply.github.com>
2020-12-02 16:29:48 +0000

    Update authDomains(new).js

--- api_server/routes/authDomains(new).js
+++ api_server/routes/authDomains(new).js
@@ -3,6 +3,19 @@ var authDomainsRouter = express.Router();
 var authDomainsHandler = require('../handlers/authDomains')
 
 /*
+
+CREATE DATABASE IF NOT EXISTS fish_and_chips;
+
+USE fish_and_chips;
+
+CREATE TABLE IF NOT EXISTS authenticated_domains (  
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    domain TEXT NOT NULL,
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+
 var mysql = require('mysql');
 var db = mysql.createConnection({
   host : 'root',
