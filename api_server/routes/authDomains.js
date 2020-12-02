9153a2f056f863c67bcdc49d3ee05f6857f1ad9c
cs20081607 <37113550+cs20081607@users.noreply.github.com>
2020-12-02 17:45:45 +0000

    Update and rename authDomains(new).js to authDomains.js

--- api_server/routes/authDomains.js
+++ api_server/routes/authDomains.js
@@ -2,31 +2,20 @@ var express = require('express');
 var authDomainsRouter = express.Router();
 var authDomainsHandler = require('../handlers/authDomains')
 
-/*
-
-CREATE DATABASE IF NOT EXISTS fish_and_chips;
-
-USE fish_and_chips;
-
-CREATE TABLE IF NOT EXISTS authenticated_domains (  
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    domain TEXT NOT NULL,
-    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
-);
-
-
 var mysql = require('mysql');
 var db = mysql.createConnection({
-  host : 'root',
+  host : 'database',
   user : 'root',
   password : 'root',
-  database : '' 
+  database : 'fish_and_chips' 
 })
 db.connect();
 
 
-*/
+
+
+
+
 
 
 authDomainsRouter.use(function timeLog(req, res, next) {
