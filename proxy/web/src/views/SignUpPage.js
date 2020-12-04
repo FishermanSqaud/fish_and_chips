e0bdefdca2337a1f0c53ead902e0b81bdfd094e5
Luke Kim <burgund32@gmail.com>
2020-12-04 18:14:41 +0900

    env test

--- proxy/web/src/views/SignUpPage.js
+++ proxy/web/src/views/SignUpPage.js
@@ -47,6 +47,8 @@ const SignUp = inject("store")(
 
         const backendUrl = process.env.REACT_APP_BACKEND_URL
 
+        console.log("backend url", backendUrl)
+
         const requestBody = {
           email: email,
           name: name,
